5126d7d4c85d01929e57c27f28c5f0225a4ffb21
Hadisahrulfauzi <147998079+Hadisahrulfauzi@users.noreply.github.com>
2024-01-02 01:30:00 +0700

    prediksi_milk.py

--- prediksi_milk.py
+++ prediksi_milk.py
@@ -66,5 +66,5 @@ if st.button("Prediksi"):
         st.write("Kualitas susu baik")
         print(hasil[0])
     else :
-        st.write("Kualitas susu baik")
+        st.write("Kualitas susu sedang")
         print(hasil[0])
