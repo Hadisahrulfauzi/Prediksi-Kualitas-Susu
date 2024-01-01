fcd6b03283c8aff8c49ccc794eb053ec8ab2be71
Hadisahrulfauzi <147998079+Hadisahrulfauzi@users.noreply.github.com>
2024-01-01 23:47:10 +0700

    prediksi_milk.py

--- prediksi_milk.py
+++ prediksi_milk.py
@@ -1,80 +1,45 @@
 import pickle
 import streamlit as st
 
+model_file_path = "prediksi_milk.sav"
 
-model = pickle.load(open("prediksi_milk.sav", "rb"))
+# Check if the model file exists
+try:
+    model = pickle.load(open(model_file_path, "rb"))
+except FileNotFoundError:
+    st.error(f"Model file '{model_file_path}' not found. Please check the file path.")
 
 st.title('Prediksi Kualitas Susu')
 st.write("Lengkapi Data dibawah ini")
 
-
 pH = st.number_input('Input Nilai pH Susu')
-Temprature = st.number_input('Input Suhu Susu')
-Taste = st.selectbox(
-    "Kualitas Rasa Susu",
-    [
-        "Baik",
-        "Buruk",
-    ],
-)
-if Taste == "Baik":
-    Taste = 1
-else:
-    Taste = 0
-Odor = st.selectbox(
-    "Kualitas Bau susu",
-    [
-        "Baik",
-        "Buruk",
-    ],
-)
-if Odor == "Baik":
-    Odor = 1
-else:
-    Odor = 0
-Fat = st.selectbox(
-    "Tingkat Kadar Lemak Susu",
-    [
-        "Rendah",
-        "Tinggi",
-    ],
-)
-if Fat == "Rendah":
-    Fat = 0
-else:
-    Fat = 1
-Turbidity = st.selectbox(
-    "Kekeruhan Susu",
-    [
-        "Rendah",
-        "Tinggi",
-    ],
-)
-if Turbidity == "Rendah":
-    Turbidity = 0
-else:
-    Turbidity = 1
-Colour = st.number_input('Input Warna Susu')
+temperature = st.number_input('Input Suhu Susu')
+
+taste_mapping = {"Baik": True, "Buruk": False}
+taste = st.selectbox("Kualitas Rasa Susu", list(taste_mapping.keys()))
+taste_value = taste_mapping[taste]
+
+odor_mapping = {"Baik": True, "Buruk": False}
+odor = st.selectbox("Kualitas Bau susu", list(odor_mapping.keys()))
+odor_value = odor_mapping[odor]
+
+fat_mapping = {"Rendah": False, "Tinggi": True}
+fat = st.selectbox("Tingkat Kadar Lemak Susu", list(fat_mapping.keys()))
+fat_value = fat_mapping[fat]
+
+turbidity_mapping = {"Rendah": False, "Tinggi": True}
+turbidity = st.selectbox("Kekeruhan Susu", list(turbidity_mapping.keys()))
+turbidity_value = turbidity_mapping[turbidity]
+
+colour = st.number_input('Input Warna Susu')
 
 if st.button("Prediksi"):
-    X = [
-        [
-            pH,
-            Temprature,
-            Taste,
-            Odor,
-            Fat,
-            Turbidity,
-            Colour,
-        ]
-    ]
+    X = [[pH, temperature, taste_value, odor_value, fat_value, turbidity_value, colour]]
     hasil = model.predict(X)
+    
     if hasil['low'] == 1:
         st.write("Kualitas susu buruk")
-        print(hasil['low'])
     elif hasil['medium'] == 1:
         st.write("Kualitas susu sedang")
-        print(hasil['medium'])
     elif hasil['high'] == 1:
         st.write("Kualitas susu baik")
-        print(hasil['high'])
